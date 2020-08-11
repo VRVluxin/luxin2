1343c1f314465a8b1f1581731d17340cda2e212c
luxin <2439814364@qq.com>
2020-08-11 18:03:22 +0800

    first commit

---
+++ test1/test1/test1.cpp
@@ -0,0 +1,90 @@
+// test1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//std::cout << " Hello ,World" << std::endl;
+	
+	//int a = 0;
+	//int b = 0;
+	//std::cout << "输入两个数" << std::endl;
+	//std::cin >> a >> b ;
+	//std::cout << a<<"*"<<b<<"=" << a*b << std::endl;
+
+	//std::cout << "9" ;
+	//std::cout << "*" ;
+	//std::cout << "6" ;
+	//std::cout << "=" ;
+	//std::cout << "54" << std::endl;
+
+	//std::cout << "9" << std::endl;
+	//std::cout << "*" << std::endl;
+	//std::cout << "6" << std::endl;
+	//std::cout << "=" << std::endl;
+	//std::cout << "54" << std::endl;
+
+	///*
+	//lx6666
+	///*
+	//lx777
+	//*/
+	//*/
+
+	//std::cout << "/*";
+	//std::cout << "*/";
+	//std::cout <</*"*/"*/;
+	//std::cout <</* "*/" /* "/*" */;
+
+	//int i = 0;
+	//int sum = 0;
+	//while (i<50)
+	//{
+	//	sum += i;
+	//	++i;
+	//}
+	//std::cout << sum << std::endl;
+
+	//int i = 10;
+	//int sum = 0;
+	//while (i>0)
+	//{
+	//	sum += i;
+	//	--i;
+	//}
+	//std::cout << sum << std::endl;
+
+	//int a = 0;
+	//int b = 0;
+	//std::cout << "输入两个由小到大的整数" << std::endl;
+	//std::cin >> a >> b;
+	//int i = a + 1;
+	//int sum = 0;
+	//while (a<i&&i<b)
+	//{
+	//	std::cout << i << std::endl;
+	//	++i;
+	//}
+
+	//int sum = 0;
+	//for (int i = -100; i <= 100; ++i)
+	//{
+	//	sum += i;
+	//}
+	//std::cout << sum << std::endl;
+	
+
+	//比较i++和++i的区别
+	for (int i = -0; i <= 10; ++i)
+	{
+		std::cout << i << std::endl;
+	}	
+	//for (int i = -0; i <= 10; i++)
+	//{
+	//	std::cout << i << std::endl;
+	//}
+	return 0;
+}
+
